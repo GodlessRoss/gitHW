ef1b2ebd293529eba26d2a14a0f363a2221424a3
AlbaRusensia <AlbaRusensia@gmail.com>
2018-05-16 00:56:51 +0300

    Комментарий

--- src/v1/Main.java
+++ src/v1/Main.java
@@ -1,9 +1,18 @@
 package v1;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
-		
+		System.out.println(
+				"Выберите команду: \n1.Включить;\n2.Выключить;\n3.Приготовить эспрессо;\n4.Приготовить американо;\n5.Очистка бака, отработанного кофе.\n");
+		Scanner scanner =  new Scanner(System.in);
+		int choice = scanner.nextInt();
+		switch(choice) {
+		case 1:
+			System.out.println();
+		}
 	}
 
 }
