0b899bc99d270ab2cfc9a809ace27209464bb975
GodlessRoss <mr.rikishiross@icloud.com>
2018-05-16 00:20:35 +0300

    Test

--- src/v1/Coffemachine.java
+++ src/v1/Coffemachine.java
@@ -3,20 +3,24 @@ package v1;
 public class Coffemachine implements MakingEspresso {
 	private int maxAmountOfGroundCoffee;
 	private int maxAmountOfWater;
-	
+
 	private int currentAmountOfGroundCoffee;
 	private int currentAmountOfWater;
-	
+
 	@Override
 	public void makeCoffe(String nameCoffe) {
+		int bufferAmountOfGroundCoffee = 0;
+		int bufferAmountOfWater = 0;
 		switch (nameCoffe) {
-		case "espresso":
-			System.out.println("Готовлю эспрессо!");
-			currentAmountOfGroundCoffee -= NEED_AMOUNT_OF_GROUND_COFFE;
-			currentAmountOfWater -= NEED_AMOUNT_OF_WATER;
-			System.out.println("Ваш эспрессо готов!");
+		case "эспрессо":
+			bufferAmountOfGroundCoffee = MakingEspresso.NEED_AMOUNT_OF_GROUND_COFFE;
+			bufferAmountOfWater = MakingEspresso.NEED_AMOUNT_OF_WATER;
 			break;
 		}
+		System.out.println("Начинаю готовить " + nameCoffe);
+		currentAmountOfGroundCoffee -= bufferAmountOfGroundCoffee;
+		currentAmountOfWater -= currentAmountOfWater;
+		System.out.println("Ваш " + nameCoffe + " готов.");
 	}
 
 }
