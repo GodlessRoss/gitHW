9428c3658826d046ddaee7583fec3decb6d0c7a7
AlbaRusensia <AlbaRusensia@gmail.com>
2018-05-31 22:17:34 +0300

    Ком

--- Directory/src/view/MainFrame.java
+++ Directory/src/view/MainFrame.java
@@ -5,6 +5,25 @@ import java.awt.event.*;
 import javax.swing.*;
 
 public class MainFrame extends JFrame {
-
+	private JButton buttin = new JButton("Тест");
+	private JButton buttin1 = new JButton("Альманах");
+	private JButton buttin3 = new JButton("Войти администратору");
+	private JButton buttin2 = new JButton("Выйти");
+	
+	
+	public  MainFrame () {
+		super("Simpel Example");
+		this.setBounds(500,500,500,500);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		Container container =  this.getContentPane();
+		container.setLayout(new GridLayout(3,2,2,2));
+		container.add(buttin);
+		container.add(buttin1);
+		container.add(buttin3);
+		container.add(buttin2);
+		
+	}
+	
 
 }
