136090f83f0f4b7f36a0609ba35385fbcc7e2820
GodlessRoss <mr.rikishiross@icloud.com>
2018-06-01 21:39:28 +0300

    add medoth add Test

--- Directory/src/controller/Helper.java
+++ Directory/src/controller/Helper.java
@@ -1,17 +1,21 @@
 package controller;
 
 import model.Almanac;
+import model.Question;
+import model.Test;
 import model.Theme;
 
 public final class Helper {
 	static Almanac almanac;
 	static Theme[] themes;
-	
+	static Test[] tests;
+
 	static {
 		almanac = Runner.getAlmanac();
 		themes = almanac.getThemes();
+		tests = almanac.getTests();
 	}
-	
+
 	public static String setingText(String name) {
 		for (Theme theme : themes) {
 			if (name != null && name.equals(theme.getName())) {
@@ -19,5 +23,22 @@ public final class Helper {
 			}
 		}
 		return "Ошибка чтения";
+	}
+
+	public static Test какоетоНазваниеМетода(String name) {
+		Question[] quest;
+		for (Test test : tests) {
+			if (name != null && name.equals(test.getQuestions())) {
+				quest = test.getQuestions();
+				for (Question questmads : quest) {
+					questmads.getName();
+					questmads.getText();
+					String prav = questmads.getCorrectAnswer();
+					
+				}
+				return test;
+			}
+		}
+		return null;
 	}
 }
