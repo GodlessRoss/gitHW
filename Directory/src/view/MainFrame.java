5ad27fce31e9a32edd9bdd78e1dfa30f65440bd1
GodlessRoss <mr.rikishiross@icloud.com>
2018-06-01 19:02:30 +0300

    add medoth add Test

--- Directory/src/view/MainFrame.java
+++ Directory/src/view/MainFrame.java
@@ -7,8 +7,6 @@ import javax.swing.GroupLayout.Alignment;
 import javax.swing.tree.DefaultTreeModel;
 
 import controller.Helper;
-import controller.Runner;
-import model.Almanac;
 
 import javax.swing.tree.DefaultMutableTreeNode;
 import javax.swing.LayoutStyle.ComponentPlacement;
@@ -18,10 +16,18 @@ import javax.swing.event.TreeSelectionListener;
 public class MainFrame extends JFrame {
 
 	private JTree themeTree;
+	private JTextArea themeTextArea;
+	private JScrollBar treeScrollBar;
+	private JButton addTestButton;
+	private JButton runTestButton;
+	private JScrollPane themeScrollPane;
+	private JSeparator separator;
+	private JScrollPane scrollPaneTree;
 
-	{
-		Almanac almanac = Runner.getAlmanac();
-	}
+	private int counter = 1;
+	private JLabel lblNewLabel;
+	private JScrollPane addTestScrollPane;
+	private JTextArea addTestTextArea;
 
 	/**
 	 * Create the application.
@@ -31,80 +37,90 @@ public class MainFrame extends JFrame {
 		setBounds(100, 100, 587, 281);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-		JScrollPane scrollPaneTree = new JScrollPane();
+		scrollPaneTree = new JScrollPane();
 		scrollPaneTree.setBounds(0, 0, 500, 300);
 		getContentPane().add(scrollPaneTree);
 
-		JSeparator separator = new JSeparator();
+		separator = new JSeparator();
 		separator.setOrientation(SwingConstants.VERTICAL);
 		separator.setBackground(Color.BLACK);
 
-		JScrollPane scrollPane = new JScrollPane();
+		themeScrollPane = new JScrollPane();
 
-		JButton button = new JButton("Начать тест");
-		button.addActionListener(new ActionListener() {
+		runTestButton = new JButton("Начать тест");
+		runTestButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 			}
 		});
 
-		JButton button_1 = new JButton("Добавить в список");
-		button_1.addActionListener(new ActionListener() {
+		addTestButton = new JButton("Добавить в список");
+		addTestButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
+				DefaultMutableTreeNode selectedNode = (DefaultMutableTreeNode) themeTree.getLastSelectedPathComponent();
+				if (selectedNode != null) {
+					String selectedObj = selectedNode.getUserObject().toString();
+					if (addTestTextArea.getText().indexOf(selectedObj) == -1) {
+						addTestTextArea
+								.setText(addTestTextArea.getText() + counter++ + ". " + selectedObj.toString() + ";\n");
+					}
+				}
 			}
 		});
 
-		JPanel panel = new JPanel();
-		panel.setBackground(Color.WHITE);
+		lblNewLabel = new JLabel("Выбранные тесты");
+
+		addTestScrollPane = new JScrollPane();
 		GroupLayout groupLayout = new GroupLayout(getContentPane());
-		groupLayout.setHorizontalGroup(
-			groupLayout.createParallelGroup(Alignment.LEADING)
+		groupLayout.setHorizontalGroup(groupLayout.createParallelGroup(Alignment.LEADING)
 				.addGroup(groupLayout.createSequentialGroup()
-					.addComponent(scrollPaneTree, GroupLayout.PREFERRED_SIZE, 160, GroupLayout.PREFERRED_SIZE)
-					.addPreferredGap(ComponentPlacement.RELATED)
-					.addComponent(separator, GroupLayout.PREFERRED_SIZE, 6, GroupLayout.PREFERRED_SIZE)
-					.addPreferredGap(ComponentPlacement.RELATED)
-					.addComponent(scrollPane, GroupLayout.DEFAULT_SIZE, 234, Short.MAX_VALUE)
-					.addGap(18)
-					.addGroup(groupLayout.createParallelGroup(Alignment.LEADING, false)
-						.addComponent(panel, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-						.addComponent(button_1, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-						.addComponent(button))
-					.addGap(12))
-		);
-		groupLayout.setVerticalGroup(
-			groupLayout.createParallelGroup(Alignment.LEADING)
-				.addGroup(groupLayout.createSequentialGroup()
-					.addContainerGap()
-					.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
-						.addComponent(scrollPane, Alignment.TRAILING, GroupLayout.DEFAULT_SIZE, 213, Short.MAX_VALUE)
-						.addComponent(separator, GroupLayout.DEFAULT_SIZE, 213, Short.MAX_VALUE)
-						.addComponent(scrollPaneTree, Alignment.TRAILING, GroupLayout.DEFAULT_SIZE, 213, Short.MAX_VALUE)
-						.addGroup(Alignment.TRAILING, groupLayout.createSequentialGroup()
-							.addComponent(panel, GroupLayout.DEFAULT_SIZE, 145, Short.MAX_VALUE)
-							.addPreferredGap(ComponentPlacement.UNRELATED)
-							.addComponent(button)
-							.addPreferredGap(ComponentPlacement.UNRELATED)
-							.addComponent(button_1)))
-					.addContainerGap())
-		);
-		panel.setLayout(new BorderLayout(0, 0));
+						.addComponent(scrollPaneTree, GroupLayout.PREFERRED_SIZE, 160, GroupLayout.PREFERRED_SIZE)
+						.addPreferredGap(ComponentPlacement.RELATED)
+						.addComponent(separator, GroupLayout.PREFERRED_SIZE, 6, GroupLayout.PREFERRED_SIZE)
+						.addPreferredGap(ComponentPlacement.RELATED)
+						.addComponent(themeScrollPane, GroupLayout.DEFAULT_SIZE, 244, Short.MAX_VALUE)
+						.addPreferredGap(ComponentPlacement.UNRELATED)
+						.addGroup(groupLayout.createParallelGroup(Alignment.LEADING, false).addComponent(lblNewLabel)
+								.addComponent(addTestButton, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE,
+										Short.MAX_VALUE)
+								.addComponent(runTestButton).addComponent(addTestScrollPane))
+						.addContainerGap()));
+		groupLayout.setVerticalGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+				.addGroup(groupLayout.createSequentialGroup().addContainerGap()
+						.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+								.addComponent(themeScrollPane, Alignment.TRAILING, GroupLayout.DEFAULT_SIZE, 220,
+										Short.MAX_VALUE)
+								.addGroup(groupLayout.createSequentialGroup().addComponent(lblNewLabel)
+										.addPreferredGap(ComponentPlacement.RELATED)
+										.addComponent(addTestScrollPane, GroupLayout.DEFAULT_SIZE, 136, Short.MAX_VALUE)
+										.addPreferredGap(ComponentPlacement.UNRELATED).addComponent(runTestButton)
+										.addGap(7).addComponent(addTestButton))
+								.addComponent(separator, Alignment.TRAILING, GroupLayout.DEFAULT_SIZE, 220,
+										Short.MAX_VALUE)
+								.addComponent(scrollPaneTree, Alignment.TRAILING, GroupLayout.DEFAULT_SIZE, 220,
+										Short.MAX_VALUE))
+						.addContainerGap()));
 
-		JTextArea textArea_1 = new JTextArea();
-		textArea_1.setBackground(Color.WHITE);
-		textArea_1.setWrapStyleWord(true);
-		textArea_1.setLineWrap(true);
-		textArea_1.setEditable(false);
-		panel.add(textArea_1, BorderLayout.CENTER);
+		addTestTextArea = new JTextArea();
+		addTestTextArea.setWrapStyleWord(true);
+		addTestTextArea.setLineWrap(true);
+		addTestTextArea.setEditable(false);
+		addTestTextArea.setBackground(Color.WHITE);
+		addTestScrollPane.setViewportView(addTestTextArea);
 
-		JTextArea textArea = new JTextArea();
-		scrollPane.setViewportView(textArea);
-		textArea.setWrapStyleWord(true);
-		textArea.setLineWrap(true);
-		textArea.setEditable(false);
+		themeTextArea = new JTextArea();
+		themeScrollPane.setViewportView(themeTextArea);
+		themeTextArea.setWrapStyleWord(true);
+		themeTextArea.setLineWrap(true);
+		themeTextArea.setEditable(false);
 
 		themeTree = new JTree();
 
 		themeTree.setModel(new DefaultTreeModel(new DefaultMutableTreeNode("Темы") {
+			/**
+			 * 
+			 */
+			private static final long serialVersionUID = 1L;
+
 			{
 				DefaultMutableTreeNode node_1;
 				node_1 = new DefaultMutableTreeNode("Введение в технологию Java");
@@ -113,8 +129,8 @@ public class MainFrame extends JFrame {
 				node_1.add(new DefaultMutableTreeNode("Инсталляция платформы Java"));
 				node_1.add(new DefaultMutableTreeNode("Пример написания первого приложения"));
 				node_1.add(new DefaultMutableTreeNode("Обзор существующих IDE"));
-//				add(node_1);
-//				add(new DefaultMutableTreeNode("ООП"));
+				 add(node_1);
+				 add(new DefaultMutableTreeNode("ООП"));
 			}
 		}));
 
@@ -122,30 +138,14 @@ public class MainFrame extends JFrame {
 			public void valueChanged(TreeSelectionEvent event) {
 				DefaultMutableTreeNode selectedNode = (DefaultMutableTreeNode) themeTree.getLastSelectedPathComponent();
 				Object selectedObj = selectedNode.getUserObject().toString();
-				if (selectedObj.equals("Введение в технологию Java")) {
-					textArea.setText(Helper.setingText(selectedObj.toString()));
-				} else if (selectedObj.equals("Базовые понятия технологии Java")) {
-					textArea.setText(Helper.setingText(selectedObj.toString()));
-				} else if (selectedObj.equals("Введение в технологию Java")) {
-					textArea.setText(Helper.setingText(selectedObj.toString()));
-				} else if (selectedObj.equals("Жизненный цикл, компиляции и исполнения  приложения Java")) {
-					textArea.setText(Helper.setingText(selectedObj.toString()));
-				} else if (selectedObj.equals("Инсталляция платформы Java")) {
-					textArea.setText(Helper.setingText(selectedObj.toString()));
-				} else if (selectedObj.equals("Пример написания первого приложения")) {
-					textArea.setText(Helper.setingText(selectedObj.toString()));
-				} else if (selectedObj.equals("Обзор существующих IDE")) {
-					textArea.setText(Helper.setingText(selectedObj.toString()));
-				} else if (selectedObj.equals("ООП")) {
-					textArea.setText(Helper.setingText(selectedObj.toString()));
-				}
+				themeTextArea.setText(Helper.setingText(selectedObj.toString()));
 			}
 		});
 
 		scrollPaneTree.setViewportView(themeTree);
 
-		JScrollBar scrollBar = new JScrollBar();
-		scrollPaneTree.setRowHeaderView(scrollBar);
+		treeScrollBar = new JScrollBar();
+		scrollPaneTree.setRowHeaderView(treeScrollBar);
 		getContentPane().setLayout(groupLayout);
 	}
 }
